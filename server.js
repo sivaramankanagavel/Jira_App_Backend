a5a260cd5fb33cc3f5114b2c8210ddf83ede02e0
Sivaraman_React_Js <sivaramankanagavel@gmail.com>
2025-07-04 17:51:05 +0530

    s3 bucket url was updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(json());
 // In server.js (backend)
 app.use(cors({
-  origin: "http://localhost:3000", // or your frontend URL
+  origin: ["http://localhost:3000", "http://tiger-task-tracker.s3-website-us-east-1.amazonaws.com"],
   credentials: true
 }));
 
