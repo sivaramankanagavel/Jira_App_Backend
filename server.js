efe8c2f864c3a1fe99ecee3e5cd75177069b7f59
Sivaraman_React_Js <sivaramankanagavel@gmail.com>
2025-07-07 06:03:47 +0530

    the server port was changed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {
